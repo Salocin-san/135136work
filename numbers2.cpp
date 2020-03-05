092df9b8d3e377ad4560b59d1117ef3a7344a7c2
Salocin-san <56706932+Salocin-san@users.noreply.github.com>
2020-03-05 15:10:03 -0500

    Create numbers2.cpp

---
+++ numbers2.cpp
@@ -0,0 +1,70 @@
+//Name: Nicolas Wong
+
+//Course: CSCI-136
+
+//Instructor: Minh Nguyen
+
+//Assignment: Lab 5D
+
+//returns number of prime numbers in given interval
+
+#include <iostream>
+
+using namespace std;
+
+bool isDivisibleBy(int n, int d);//prototype
+
+bool isPrime(int n)
+{
+
+	if (n <= 1){
+		return false;
+	}
+
+	for (int i = 2; i <= n/2; i++){
+		if (isDivisibleBy(n, i)){
+			return false;
+		}
+	}
+		return true;
+}
+
+int nextPrime(int n);//prototype
+	
+int main()
+{
+ 
+    int n;
+    cout << "Enter number: ";
+    cin >> n; 
+       
+    int answer = nextPrime(n);//gives next prime number in chronological order
+
+	cout << answer;
+
+}	
+
+bool isDivisibleBy(int n, int d)
+{
+        if (d == 0){
+            return false;//indivisible
+        }
+        
+        else if (n % d == 0){//tests if remainder is 0
+            return true;
+        }
+	
+	return false;
+}
+
+int nextPrime(int n)   
+{
+	//int n;
+	//cin >> n;
+	
+	int nextPrime = n + 1;
+	while (!isPrime(nextPrime)){
+		nextPrime++;
+	}
+	return nextPrime;
+}
